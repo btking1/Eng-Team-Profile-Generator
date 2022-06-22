2b724993e132e7d17e91147a752776939a69fa69
Booker King <btking1@my.waketech.edu>
2022-06-22 17:33:19 -0400

    initial question setup

--- index.js
+++ index.js
@@ -1,80 +1,78 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
-const Manager = require('../lib/Manager');
-const Engineer = require('../lib/Engineer');
-const Intern = require('../lib/Intern');
-const { constants } = require('buffer');
-
-const employeeQuestions = [
-    { 
-        name: 'name', 
-    message: 'What is your name?'
- },
- {
-    name: 'email', 
-    message: 'What is your email address?'
- },
- {
-    name: 'id',
-    message: 'What is your ID number?'
- }
-];
+import inquirer from "inquirer";
+import { Intern } from "./lib/Intern.js";
+import { Engineer } from "./lib/Engineer.js";
+import { Manager } from "./lib/Manager.js";
 
 
-const employees = [];
-const manager = new Manager("John", 1, "@gmail.com", "123");
-const engineer = new Engineer("John", 1, "@gmail.com", "johndoe");
-const intern = new Intern("John", 1, "@gmail.com", "UNC");
-employees.push(manager);
-employees.push(engineer);
-employees.push(intern);
-
-// Write code to use inquirer to:
-// 1. Prompt the user to select an option
-// 2. Once the user has selected an option, use switch/case to execute the option
-// 3. After the user has selected an option, use inquirer to prompt the user with the corresponding questions
-// 4. Once the user has provided answers for the questions, use the answers to create an HTML page using the HTML template provided
-// 5. Finally, open the HTML page in a browser to view the rendered HTML
-
-function start(){
-    console.log("Welcome to the Team Generator!");
-    mainmenu();
+// Questions for all employees
+async function employeeQuestion() {
+  const res = await inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is the employee's name?",
+    },
+    {
+      type: "input",
+      name: "id",
+      message: "What is the employee's id?",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is the employee's email?",
+    },
+  ])
 }
 
-async function mainmenu() {
-    const mainmenu = await inquirer.prompt(
-        {
-            type: "list",
-            name: "mainmenu",
-            message: "What would you like to do?",
-            choices: ['add manager', 'add engineer', 'add intern', 'view team', 'exit']
-        })
-    switch (mainmenu.mainmenu) {
-        case 'add manager':
-            addManager();
-            break;
-        case 'add engineer':
-            addEngineer();
-            break;
-        case 'add intern':
-            addIntern();
-            break;
-        case 'view team':
-            viewTeam();
-            break;
-        case 'exit':
-            exit();
-            break;
+// Main Menu
+async function mainMenu() {
+  console.log("Welcome to the Employee Team Generator!");
+
+  const response = await inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "role",
+        message: "What is the employee's role?",
+        choices: ["Manager", "Engineer", "Intern"],
+      },
+    ])
+    .then(function (response) {
+      if (response.role === "Manager") {
+        manQuestion();
+      } else if (response.role === "Engineer") {
+        engQuestion();
+      } else if (response.role === "Intern") {
+        intQuestion();
+      }
     }
-};
+)}
 
-//create async function that adds manager function
-
-async function addManager () {
+//Manager question for office number
+function manQuestion() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "officeNumber",
+        message: "What is the manager's office number?",
+      }
+    ])
+    .then(function (response) {
+      const manager = new Manager(
+        response.name,
+        response.id,
+        response.email,
+        response.officeNumber
+      );
+      console.log(manager);
+    });
 }
 
-async function addEngineer () {}
+// Intern question for school Here
+// Engineer question for github account Here
 
-async function addIntern () {}
+mainMenu();
 
-function finish () {}
+
